e35752ee707912bd226eb380caf21bb8204d34ad
Sandro Jordão <sandro.jordao@wolk.com.br>
2022-04-28 15:45:35 -0300

    init dev vote

--- js/default.js
+++ js/default.js
@@ -29,9 +29,9 @@ $(document).ready(() => {
 		clickNumber($(this).attr("data-valor")) 
 	})
 
-	$("#btn-branco").click(clickBlank)
-	$("#btn-corrige").click(clickCorrect)
-	$("#btn-confirma").click(clickConfirm)
+	$("#btn-blank").click(clickBlank)
+	$("#btn-correct").click(clickCorrect)
+	$("#btn-confirm").click(clickConfirm)
 	$("#btn-reset").click(reset)
 
 	setTimeout(() => {
@@ -44,9 +44,14 @@ const inputAnimate = () => {
 	$(inputs[current]).addClass('input-animate')
 }
 
+const validClick = () => {
+	return ['loading', 'end'].includes(screen)
+}
+
 const clickNumber = (number) => {
-	if (screen == 'end') return false
+	if (validClick()) return false
 	if (len_numbers < (current + 1)) return false
+	console.log('Click Number: ' + number)
 	$(inputs[current]).val(number)
 	current++
 	inputAnimate()
@@ -55,14 +60,14 @@ const clickNumber = (number) => {
 }
 
 const clickBlank = () => {
-	if (screen == 'end') return false
-
+	if (validClick()) return false
+	console.log('Click Blank')
 	screenTransition('blank')
 }
 
 const clickCorrect = () => {
-	if (screen == 'end') return false
-
+	if (validClick()) return false
+	console.log('Click Correct')
 	inputs.each(function() {
 		$(this).val('')
 	})
@@ -77,26 +82,37 @@ const clickCorrect = () => {
 }
 
 const clickConfirm = () => {
-	if (screen == 'end') return false
+	if (validClick()) return false
+	if (screen == 'valid' && len_numbers != current) return false
+	$("#btn-confirm").prop("disabled", true)
+	console.log('Click Confirm')
 
 	/* Refatorar Aqui ... */
 	if (screen == 'blank') {
 		num_branco++
 		$("#number-branco").html(num_branco)
+		save(2)
 	} else if (screen == 'null') {
 		num_nulo++
 		$("#number-nulo").html(num_nulo)
+		save(3)
 	} else if (screen == 'valid' && len_numbers == current) {
 		let office = numbers.join('')
 		$("#number-" + office).html( parseInt($("#number-" + office).html()) + 1 )
+		save(1)
 	}
 	/* *** */
 
 	screenTransition('loading')
-	setTimeout(() => { screenTransition('end') }, 1000)
+	setTimeout(() => { 
+		screenTransition('end')
+		$("#btn-confirm").prop("disabled", false)
+	}, 1000)
 }
 
 const screenTransition = (show, callback) => {
+	if (screen == show) return false
+	console.log('Transition: ' + show)
 	callback = (typeof callback == 'function') ? callback : () => false;
 	$("#screen-" + screen).hide('fade', 300, () => {
 		callback()
@@ -125,18 +141,19 @@ const setInputNumbersNull = () => {
 
 const setDataVoteValid = () => {
 	let office = numbers.join('')
+	var id = $("#office-" + office).attr('data-id')
 	var name = $("#office-" + office).attr('data-name')
 	var image = $("#office-" + office).attr('data-image')
 	var vice_name = $("#office-" + office).attr('data-vice-name')
 	var vice_image = $("#office-" + office).attr('data-vice-image')
 
-	$(".visible").removeClass('hidden').addClass('show')
-
+	$("#candidato_id").val(id)
 	$("#office-name").html(name)
 	$("#office-img").html('<img src="img/'+ image +'" />')
-
 	$("#office-name-vice").html(vice_name)
 	$("#office-img-vice").html('<img src="img/'+ vice_image +'" />')
+
+	$(".visible").removeClass('hidden').addClass('show')
 }
 
 const reset = () => {
@@ -149,4 +166,19 @@ const reset = () => {
 	setTimeout(() => { screenTransition('valid') }, 1000)
 }
 
-function save() {}
+function save(situacao) {
+	var escola_id = $("#escola_id").val()
+	var escola_nome = $("#escola_nome").val()
+	var campanha_id = $("#campanha_id").val()
+	var candidato_id = situacao == 1 ? $("#candidato_id").val() : ''
+	
+	var post = { 
+		escola_id, 
+		escola_nome, 
+		campanha_id, 
+		candidato_id, 
+		situacao 
+	}
+
+	console.table(post)
+}
